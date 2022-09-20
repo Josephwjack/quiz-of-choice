54f5e0e107256145ca3b95f283004090760a8fc1
Joseph Jack <joseph.w.jack@gmail.com>
2022-09-20 13:30:46 -0700

    add edit quiz form

---
+++ src/components/EditQuizForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditQuizForm(props) {
+  const { quiz } = props;
+
+  function handleEditQuizFormSubmission(event) {
+    event.preventDefault();
+    props.onEditQuiz({
+      name: event.target.name.value,
+      question1: event.target.question1.value,
+      question2: event.target.question2.value,
+      question3: event.target.question3.value
+    });
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditQuizFormSubmission}
+        buttonText="Update Quiz" />
+    </React.Fragment>
+  );
+}
+
+EditQuizForm.propTypes = {
+  onEditQuiz: PropTypes.func,
+  quiz: PropTypes.object
+};
+
+export default EditQuizForm;
